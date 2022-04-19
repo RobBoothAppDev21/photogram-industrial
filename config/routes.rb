d2ebc117b2a4136e357e447794b4365e8e22a6cc
Rob Young <rhyoung214@gmail.com>
2022-04-18 20:05:24 -0500

    update routes with root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'photos#index'
+
+  devise_for :users
+
+  resources :comments
   resources :follow_requests
   resources :likes
-  resources :comments
   resources :photos
-  root 'photos#index'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
