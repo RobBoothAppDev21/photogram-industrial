70ea37a8e9fa464a412aa17befa92e671e56cf0d
Rob Young <rhyoung214@gmail.com>
2022-04-18 21:36:31 -0500

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show # get "/users/:id" => "users#show", as: :user
 end
