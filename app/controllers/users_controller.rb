ae6f8487b79dfc6562210139735584240c622ad8
Rob Young <rhyoung214@gmail.com>
2022-04-19 16:12:05 -0500

    implemented feed, followers, and following functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show feed followers following]
+
   def show
+  end
+
+  def feed
+  end
+
+  def followers
+  end
+
+  def following
+  end
+
+  private
+
+  def set_user
     @user = User.find_by!(username: params.fetch(:username))
   end
 end
